bdb5e12f81f614018a69f4491a244aa0189bd28e
Felix Bernhardt <felix1.bernhardt@st.ovgu.de>
2021-11-23 13:55:36 +0100

    Menu

---
+++ MyHouseKeepingBook/Menu.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Forms;
+
+namespace MyHouseKeepingBook {
+    public partial class Menu : Form {
+        public Menu() {
+            InitializeComponent();
+        }
+    }
+}
+
